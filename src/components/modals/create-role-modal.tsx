b5945e3805c7acde2140e42c9d3907e08a7febb7
HsxMark <hsxmark@outlook.com>
2024-12-06 01:13:36 +0800

    fix(modals): fix create-role-modal locales and modal form

--- src/components/modals/create-role-modal.tsx
+++ src/components/modals/create-role-modal.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Icon,
   Input,
   Link,
@@ -6,11 +7,19 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { LuLink2Off, LuServer } from "react-icons/lu";
+import { LuLink2Off, LuPlus, LuServer } from "react-icons/lu";
 import SegmentedControl from "@/components/common/segmented";
 
 interface AuthSource {
@@ -25,6 +34,7 @@ const CreateRoleModal: React.FC<CreateRoleModalProps> = ({
   authSources = [],
 }) => {
   const { t } = useTranslation();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedRole, setSelectedRole] = useState<string>("offline");
   const [rolename, setRolename] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -46,77 +56,101 @@ const CreateRoleModal: React.FC<CreateRoleModalProps> = ({
   const hasAuthSources = authSources.length > 0;
 
   return (
-    <div>
-      <SegmentedControl
-        selected={selectedRole}
-        onSelectItem={(s) => setSelectedRole(s)}
-        size="2xs"
-        items={roleTypeList.map((item) => ({
-          ...item,
-          label: item.key,
-          value: <Icon as={item.icon} />,
-        }))}
-        withTooltip={true}
-      />
-
-      <div style={{ marginTop: "1rem" }}>
-        {selectedRole === "offline" ? (
-          <>
-            <Input
-              placeholder={t("CreateRoleModal.enterRolename")}
-              value={rolename}
-              onChange={(e) => setRolename(e.target.value)}
-              required
+    <>
+      <Button leftIcon={<LuPlus />} onClick={onOpen} size="sm">
+        {t("CreateRoleModal.modal.create")}
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{t("CreateRoleModal.modal.header")}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <SegmentedControl
+              selected={selectedRole}
+              onSelectItem={(s) => setSelectedRole(s)}
+              size="2xs"
+              items={roleTypeList.map((item) => ({
+                ...item,
+                label: item.key,
+                value: <Icon as={item.icon} />,
+              }))}
+              withTooltip={true}
             />
-          </>
-        ) : (
-          <>
-            {!hasAuthSources ? (
-              <Text>
-                {t("CreateRoleModal.noAuthSource")}
-                <Link href="/add-auth-source">
-                  {t("CreateRoleModal.addAuthSource")}
-                </Link>
-              </Text>
-            ) : (
-              <>
-                <Menu>
-                  <MenuButton
-                    as={Input}
-                    value={authSource}
-                    placeholder={t("CreateRoleModal.selectAuthSource")}
+            <div style={{ marginTop: "1rem" }}>
+              {selectedRole === "offline" ? (
+                <>
+                  <Input
+                    placeholder={t("CreateRoleModal.rolename.placeholder")}
+                    value={rolename}
+                    onChange={(e) => setRolename(e.target.value)}
+                    required
                   />
-                  <MenuList>
-                    {authSources.map((source) => (
-                      <MenuItem
-                        key={source.name}
-                        onClick={() => setAuthSource(source.name)}
-                      >
-                        {source.name}
-                      </MenuItem>
-                    ))}
-                  </MenuList>
-                </Menu>
-
-                <Input
-                  placeholder={t("CreateRoleModal.enterRolename")}
-                  value={rolename}
-                  onChange={(e) => setRolename(e.target.value)}
-                  required
-                />
-                <Input
-                  placeholder={t("CreateRoleModal.enterPassword")}
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </>
-            )}
-          </>
-        )}
-      </div>
-    </div>
+                </>
+              ) : (
+                <>
+                  {!hasAuthSources ? (
+                    <Text>
+                      {t("CreateRoleModal.auth.noSource")}
+                      <Link href="/add-auth-source">
+                        {t("CreateRoleModal.auth.addSource")}
+                      </Link>
+                    </Text>
+                  ) : (
+                    <>
+                      <Menu>
+                        <MenuButton
+                          as={Input}
+                          value={authSource}
+                          placeholder={t("CreateRoleModal.auth.selectSource")}
+                        />
+                        <MenuList>
+                          {authSources.map((source) => (
+                            <MenuItem
+                              key={source.name}
+                              onClick={() => setAuthSource(source.name)}
+                            >
+                              {source.name}
+                            </MenuItem>
+                          ))}
+                        </MenuList>
+                      </Menu>
+                      <Input
+                        placeholder={t("CreateRoleModal.rolename.placeholder")}
+                        value={rolename}
+                        onChange={(e) => setRolename(e.target.value)}
+                        required
+                      />
+                      <Input
+                        placeholder={t("CreateRoleModal.password.placeholder")}
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </>
+                  )}
+                </>
+              )}
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>
+              {t("CreateRoleModal.modal.cancel")}
+            </Button>
+            <Button
+              colorScheme="blue"
+              onClick={() => {
+                onClose();
+              }}
+              ml={3}
+            >
+              {t("CreateRoleModal.modal.confirm")}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
