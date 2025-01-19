63f3997f4d01141a6909d82756c5abbf0c8cd31a
baiyuansjtu <sun060319@sjtu.edu.cn>
2025-01-19 21:09:01 +0800

    refactor(frontend): update state and callback names, move skinOptions inside, and change SkinType to an exported type

--- src/components/modals/manage-skin-modal.tsx
+++ src/components/modals/manage-skin-modal.tsx
@@ -19,44 +19,43 @@ import { useTranslation } from "react-i18next";
 import SkinPreview from "@/components/common/skin-preview";
 import { useLauncherConfig } from "@/contexts/config";
 
+export type SkinType = "default" | "steve" | "alex";
+
 interface ManageSkinModalProps extends Omit<ModalProps, "children"> {
-  initialSkin?: "default" | "steve" | "alex";
+  skin?: SkinType;
   isCenter?: boolean;
-  onOKCallback?: () => void;
+  onSelectSkin?: (skin: SkinType) => void;
 }
-
-const skinOptions = {
-  default: "/images/skins/unicorn_isla.png",
-  steve: "/images/skins/steve.png",
-  alex: "/images/skins/alex.png",
-} as const;
-
-type SkinType = keyof typeof skinOptions;
 
 const ManageSkinModal: React.FC<ManageSkinModalProps> = ({
   isOpen,
   onClose,
   isCenter = false,
-  initialSkin = "default",
-  onOKCallback,
+  skin = "default",
+  onSelectSkin,
   ...modalProps
 }) => {
-  const [skin, setSkin] = useState<SkinType>(initialSkin);
+  const [selectedSkin, setSelectedSkin] = useState<SkinType>(skin);
   const { t } = useTranslation();
   const { config } = useLauncherConfig();
   const primaryColor = config.appearance.theme.primaryColor;
+  const skinOptions = {
+    default: "/images/skins/unicorn_isla.png",
+    steve: "/images/skins/steve.png",
+    alex: "/images/skins/alex.png",
+  } as const;
 
   useEffect(() => {
-    setSkin(initialSkin);
-  }, [initialSkin]);
+    setSelectedSkin(skin);
+  }, [skin]);
 
   const handleSkinChange = (skinType: SkinType) => {
-    setSkin(skinType);
+    setSelectedSkin(skinType);
   };
 
   const handleSave = () => {
-    if (onOKCallback) {
-      onOKCallback();
+    if (onSelectSkin) {
+      onSelectSkin(selectedSkin);
     }
     onClose();
   };
@@ -76,15 +75,15 @@ const ManageSkinModal: React.FC<ManageSkinModalProps> = ({
           <Grid templateColumns="3fr 2fr" gap={4} h="320px">
             <Flex justify="center" align="center" width="100%" height="100%">
               <SkinPreview
-                skinSrc={skinOptions[skin]}
+                skinSrc={skinOptions[selectedSkin]}
                 width={270}
                 height={310}
                 showControlBar
               />
             </Flex>
 
-            <RadioGroup value={skin} onChange={handleSkinChange}>
-              <VStack spacing={4}>
+            <RadioGroup value={selectedSkin} onChange={handleSkinChange}>
+              <VStack spacing={4} alignItems="flex-start">
                 {Object.keys(skinOptions).map((key) => (
                   <Radio key={key} value={key} colorScheme={primaryColor}>
                     {t(`ManageSkinModal.${key}`)}
