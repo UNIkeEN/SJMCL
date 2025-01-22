e3d1bdaf5b8aa79c2445bfced6cdedcbee40c003
HsxMark <hsxmark@outlook.com>
2025-01-22 22:29:32 +0800

    feat(frontend): add IconButton in java page, fix locales key

--- src/pages/settings/java.tsx
+++ src/pages/settings/java.tsx
@@ -1,8 +1,22 @@
-import { Flex, IconButton, Tag, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Tag,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import { open } from "@tauri-apps/plugin-shell";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { LuFolderOpen } from "react-icons/lu";
+import {
+  LuArrowDownToLine,
+  LuFolderOpen,
+  LuPlus,
+  LuRefreshCcw,
+} from "react-icons/lu";
 import Empty from "@/components/common/empty";
 import { OptionItem, OptionItemGroup } from "@/components/common/option-item";
 import { Section } from "@/components/common/section";
@@ -14,7 +28,7 @@ const JavaInfoPage = () => {
   const [javaInfo, setJavaInfo] = useState<JavaInfo[]>([]);
   const { config } = useLauncherConfig();
   const primaryColor = config.appearance.theme.primaryColor;
-
+  const router = useRouter();
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -22,7 +36,46 @@ const JavaInfoPage = () => {
   }, []);
 
   return (
-    <Section title={t("JavaInfoPage.javaList.title")}>
+    <Section
+      title={t("JavaInfoPage.javaList.title")}
+      headExtra={
+        <HStack spacing={2}>
+          <Tooltip label={t("JavaInfoPage.javaList.download")}>
+            <IconButton
+              aria-label={t("JavaInfoPage.javaList.download")}
+              icon={<Icon as={LuArrowDownToLine} boxSize={3.5} />}
+              size="xs"
+              h={21}
+              variant="ghost"
+              colorScheme="gray"
+            ></IconButton>
+          </Tooltip>
+          <Tooltip label={t("General.refresh")}>
+            <IconButton
+              aria-label={t("General.refresh")}
+              icon={<Icon as={LuRefreshCcw} boxSize={3.5} />}
+              size="xs"
+              h={21}
+              variant="ghost"
+              colorScheme="gray"
+              onClick={() => {
+                router.push("/settings/java");
+              }}
+            ></IconButton>
+          </Tooltip>
+          <Tooltip label={t("JavaInfoPage.javaList.add")}>
+            <IconButton
+              aria-label={t("JavaInfoPage.javaList.add")}
+              icon={<Icon as={LuPlus} boxSize={3.5} />}
+              size="xs"
+              h={21}
+              variant="ghost"
+              colorScheme="gray"
+            ></IconButton>
+          </Tooltip>
+        </HStack>
+      }
+    >
       {javaInfo.length > 0 ? (
         <OptionItemGroup
           items={javaInfo.map((info) => (
@@ -32,7 +85,11 @@ const JavaInfoPage = () => {
               description={info.fileDir}
               titleExtra={
                 <Flex alignItems={"center"}>
-                  <Tag size="sm" variant="subtle" colorScheme={primaryColor}>
+                  <Tag
+                    fontSize="2xs"
+                    variant="subtle"
+                    colorScheme={primaryColor}
+                  >
                     {info.version}
                   </Tag>
                   <Text fontSize="xs" color={`${primaryColor}.500`} ml={2}>
