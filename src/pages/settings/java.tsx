d0e2048eb31a88d1407cdc5a6d56c1a6d4a9b277
HsxMark <hsxmark@outlook.com>
2025-01-22 20:43:01 +0800

    feat(frontend): add java management page, add system-info

--- src/pages/settings/java.tsx
+++ src/pages/settings/java.tsx
@@ -1,28 +1,66 @@
+import { Flex, IconButton, Tag, Text, Tooltip } from "@chakra-ui/react";
+import { open } from "@tauri-apps/plugin-shell";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import ComingSoonSign from "@/components/coming-soon";
-import {
-  OptionItemGroup,
-  OptionItemGroupProps,
-} from "@/components/common/option-item";
+import { LuFolderOpen } from "react-icons/lu";
+import Empty from "@/components/common/empty";
+import { OptionItem, OptionItemGroup } from "@/components/common/option-item";
+import { Section } from "@/components/common/section";
+import { useLauncherConfig } from "@/contexts/config";
+import { mockJavaInfo } from "@/models/mock/system-info";
+import { JavaInfo } from "@/models/system-info";
 
-const JavaSettingsPage = () => {
+const JavaInfoPage = () => {
+  const [javaInfo, setJavaInfo] = useState<JavaInfo[]>([]);
+  const { config } = useLauncherConfig();
+  const primaryColor = config.appearance.theme.primaryColor;
+
   const { t } = useTranslation();
 
-  const javaSettingGroups: OptionItemGroupProps[] = [
-    {
-      title: t("JavaSettingsPage.java.title"),
-      items: [],
-    },
-  ];
+  useEffect(() => {
+    setJavaInfo(mockJavaInfo);
+  }, []);
 
   return (
-    <>
-      {javaSettingGroups.map((group, index) => (
-        <OptionItemGroup title={group.title} items={group.items} key={index} />
-      ))}
-      <ComingSoonSign />
-    </>
+    <Section title={t("JavaInfoPage.javaList.title")}>
+      {javaInfo.length > 0 ? (
+        <OptionItemGroup
+          items={javaInfo.map((info) => (
+            <OptionItem
+              key={info.name}
+              title={info.name}
+              description={info.fileDir}
+              titleExtra={
+                <Flex alignItems={"center"}>
+                  <Tag size="sm" variant="subtle" colorScheme={primaryColor}>
+                    {info.version}
+                  </Tag>
+                  <Text fontSize="xs" color={`${primaryColor}.500`} ml={2}>
+                    {info.architecture}
+                  </Text>
+                  <Text fontSize="xs" color={`${primaryColor}.500`} ml={2}>
+                    {info.vendor}
+                  </Text>
+                </Flex>
+              }
+            >
+              <Tooltip label={t("General.openFolder")}>
+                <IconButton
+                  aria-label={t("General.openFolder")}
+                  icon={<LuFolderOpen />}
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => open(info.fileDir)}
+                />
+              </Tooltip>
+            </OptionItem>
+          ))}
+        />
+      ) : (
+        <Empty withIcon={false} size="sm" />
+      )}
+    </Section>
   );
 };
 
-export default JavaSettingsPage;
+export default JavaInfoPage;
