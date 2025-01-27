c4e8c6336435f2f04a1e1349fcfa8c3118529109
baiyuansjtu <sun060319@sjtu.edu.cn>
2025-01-27 11:56:59 +0800

    refactor(frontend): adjust code and locales

--- src/pages/standalone/game-error.tsx
+++ src/pages/standalone/game-error.tsx
@@ -1,7 +1,6 @@
 import {
   Button,
   Divider,
-  Flex,
   HStack,
   Stat,
   StatLabel,
@@ -9,55 +8,44 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { arch, platform } from "@tauri-apps/plugin-os";
-import React from "react";
 import { useTranslation } from "react-i18next";
 import { useLauncherConfig } from "@/contexts/config";
-import { createWindow } from "@/utils/window";
 
 const GameErrorPage: React.FC = () => {
   const { config } = useLauncherConfig();
   const primaryColor = config.appearance.theme.primaryColor;
   const { t } = useTranslation();
-  const gameInfo = {
+  const BasicInfo = {
     launcherVersion: config.version,
-    gameVersion: "1.20.1 Fabric",
-    physicalMemory: "16274 MB",
-    gameMemory: "6518 MB",
-    javaVersion: "21.0.3",
     os: platform()
       .replace("bsd", "BSD")
-      .replace("os", "OS") //macos
-      .replace("\w", (match) => match.toUpperCase()),
+      .replace(/\w/, (match) => match.toUpperCase())
+      .replace("Macos", "macOS"),
     architecture: arch(),
-    minecraftVersion: "1.20.1",
-    fabricVersion: "0.15.6",
-    gamePath: "G:\\Minecraft\\.minecraft",
-    javaPath: "D:\\Program Files\\Zulu\\zulu-21\\bin\\java.exe",
-    crashReason: "当前游戏因为代码不完整，无法继续运行。",
   };
 
-  const PCData = [
-    { label: "launcherVersion", value: gameInfo.launcherVersion },
-    { label: "gameVersion", value: gameInfo.gameVersion },
-    { label: "physicalMemory", value: gameInfo.physicalMemory },
-    { label: "gameMemory", value: gameInfo.gameMemory },
-    { label: "java", value: gameInfo.javaVersion },
-    { label: "os", value: gameInfo.os },
-    { label: "architecture", value: gameInfo.architecture },
+  const PCInfo = [
+    { label: "launcherVersion", value: BasicInfo.launcherVersion },
+    { label: "gameVersion", value: "1.20.1 Fabric" },
+    { label: "physicalMemory", value: "16274 MB" },
+    { label: "gameMemory", value: "6518 MB" },
+    { label: "java", value: "21.0.3" },
+    { label: "os", value: BasicInfo.os },
+    { label: "architecture", value: BasicInfo.architecture },
   ];
 
-  const gameData = [
-    { label: "minecraft", value: gameInfo.minecraftVersion },
-    { label: "fabric", value: gameInfo.fabricVersion },
+  const gameInfo = [
+    { label: "minecraft", value: "1.20.1" },
+    { label: "fabric", value: "0.15.6" },
   ];
 
   return (
     <VStack align="stretch" spacing={6} w="full" p={6}>
       <HStack justify="space-between" spacing={6}>
-        {PCData.map((item, index) => (
+        {PCInfo.map((item, index) => (
           <Stat key={index}>
             <StatLabel fontWeight="bold" fontSize="md">
-              {t(`GameErrorPage.${item.label}`)}
+              {t(`GameErrorPage.PCInfo.${item.label}`)}
             </StatLabel>
             <StatNumber fontWeight="normal" fontSize="sm">
               {item.value}
@@ -68,10 +56,10 @@ const GameErrorPage: React.FC = () => {
       <Divider />
 
       <HStack justify="flex-start">
-        {gameData.map((item, index) => (
+        {gameInfo.map((item, index) => (
           <Stat key={index} textAlign="left">
             <StatLabel fontWeight="bold" fontSize="md">
-              {t(`GameErrorPage.${item.label}`)}
+              {t(`GameErrorPage.gameInfo.${item.label}`)}
             </StatLabel>
             <StatNumber fontWeight="normal" fontSize="sm">
               {item.value}
@@ -83,42 +71,43 @@ const GameErrorPage: React.FC = () => {
 
       <Stat>
         <StatLabel fontWeight="bold" fontSize="md">
-          {t("GameErrorPage.gamePath")}
+          {t("GameErrorPage.gameInfo.gamePath")}
         </StatLabel>
         <StatNumber fontWeight="normal" fontSize="sm">
-          {gameInfo.gamePath}
+          {"G:\\Minecraft\\.minecraft"}
         </StatNumber>
       </Stat>
       <Divider />
 
       <Stat>
         <StatLabel fontWeight="bold" fontSize="md">
-          {t("GameErrorPage.javaPath")}
+          {t("GameErrorPage.PCInfo.javaPath")}
         </StatLabel>
         <StatNumber fontWeight="normal" fontSize="sm">
-          {gameInfo.javaPath}
+          {"D:\\Program Files\\Zulu\\zulu-21\\bin\\java.exe"}
         </StatNumber>
       </Stat>
       <Divider />
 
       <Stat>
         <StatLabel fontWeight="bold" fontSize="md">
-          {t("GameErrorPage.crashReason")}
+          {t("GameErrorPage.errorInfo.crashReason")}
         </StatLabel>
         <StatNumber fontWeight="normal" fontSize="sm">
-          {t("GameErrorPage.notice") + gameInfo.crashReason}
+          {t("GameErrorPage.errorInfo.notice") +
+            "当前游戏因为代码不完整，无法继续运行。"}
         </StatNumber>
       </Stat>
 
       <HStack justify="flex-start" position="sticky">
         <Button colorScheme={primaryColor} variant="solid">
-          {t("GameErrorPage.exportGameInfo")}
+          {t("GameErrorPage.button.exportGameInfo")}
         </Button>
         <Button colorScheme={primaryColor} variant="solid">
-          {t("GameErrorPage.gameLogs")}
+          {t("GameErrorPage.button.gameLogs")}
         </Button>
         <Button colorScheme={primaryColor} variant="solid">
-          {t("GameErrorPage.help")}
+          {t("GameErrorPage.button.help")}
         </Button>
       </HStack>
     </VStack>
