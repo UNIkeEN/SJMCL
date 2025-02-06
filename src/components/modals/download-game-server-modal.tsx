7a5071ee4bd6fe351fd1ddb1a314d360733c6d40
baiyuansjtu <sun060319@sjtu.edu.cn>
2025-02-06 14:16:55 +0800

    refactor(frontend): adjust code

--- src/components/modals/download-game-server-modal.tsx
+++ src/components/modals/download-game-server-modal.tsx
@@ -1,7 +1,5 @@
 import {
-  Box,
   Button,
-  Center,
   Flex,
   Modal,
   ModalBody,
@@ -11,25 +9,12 @@ import {
   ModalHeader,
   ModalOverlay,
   ModalProps,
-  Radio,
-  Step,
-  StepDescription,
-  StepIcon,
-  StepIndicator,
-  StepNumber,
-  StepSeparator,
-  StepStatus,
-  StepTitle,
-  Stepper,
-  useSteps,
 } from "@chakra-ui/react";
 import { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import GameVersionSelector from "@/components/game-version-selector";
+import { GameVersionSelector } from "@/components/game-version-selector";
 import { useLauncherConfig } from "@/contexts/config";
-import { GameDirectory } from "@/models/config";
 import { GameResourceInfo } from "@/models/resource";
-import { OptionItemGroup } from "../common/option-item";
 
 export const DownloadGameServerModal: React.FC<
   Omit<ModalProps, "children">
@@ -38,16 +23,10 @@ export const DownloadGameServerModal: React.FC<
   const { config } = useLauncherConfig();
   const primaryColor = config.appearance.theme.primaryColor;
 
-  const { activeStep, setActiveStep } = useSteps({
-    index: 0,
-    count: 2,
-  });
-
   const [selectedGameVersion, setSelectedGameVersion] =
     useState<GameResourceInfo>();
-  const [instanceDirectory, setInstanceDirectory] = useState<GameDirectory>();
 
-  const Step1Content = useMemo(() => {
+  const Content = useMemo(() => {
     return (
       <>
         <ModalBody>
@@ -63,50 +42,6 @@ export const DownloadGameServerModal: React.FC<
           <Button
             disabled={!selectedGameVersion}
             colorScheme={primaryColor}
-            onClick={() => {
-              setActiveStep(1);
-            }}
-          >
-            {t("General.next")}
-          </Button>
-        </ModalFooter>
-      </>
-    );
-  }, [modalProps.onClose, primaryColor, selectedGameVersion, setActiveStep, t]);
-
-  const Step2Content = useMemo(() => {
-    return (
-      <>
-        <ModalBody>
-          <OptionItemGroup
-            items={config.localGameDirectories.map((directory) => ({
-              title:
-                directory.name === "CURRENT_DIR"
-                  ? t(
-                      "GlobalGameSettingsPage.directories.settings.directories.currentDir"
-                    )
-                  : directory.name,
-              description: directory.dir,
-              prefixElement: (
-                <Radio
-                  isChecked={directory.dir === instanceDirectory?.dir}
-                  onChange={() => setInstanceDirectory(directory)}
-                />
-              ),
-              children: <></>,
-            }))}
-          />
-        </ModalBody>
-        <ModalFooter>
-          <Button variant="ghost" onClick={modalProps.onClose}>
-            {t("General.cancel")}
-          </Button>
-          <Button variant="ghost" onClick={() => setActiveStep(0)}>
-            {t("General.previous")}
-          </Button>
-          <Button
-            disabled={!instanceDirectory}
-            colorScheme={primaryColor}
             onClick={modalProps.onClose}
           >
             {t("General.finish")}
@@ -114,33 +49,7 @@ export const DownloadGameServerModal: React.FC<
         </ModalFooter>
       </>
     );
-  }, [
-    config.localGameDirectories,
-    instanceDirectory,
-    modalProps.onClose,
-    primaryColor,
-    setActiveStep,
-    t,
-  ]);
-
-  const steps = useMemo(
-    () => [
-      {
-        key: "server",
-        content: Step1Content,
-        description:
-          selectedGameVersion &&
-          `${selectedGameVersion.id} ${t(`GameVersionSelector.${selectedGameVersion.gameType}`)}`,
-      },
-      {
-        key: "info",
-        content: Step2Content,
-        description: "",
-      },
-    ],
-    [Step1Content, Step2Content, selectedGameVersion, t]
-  );
-
+  }, [modalProps.onClose, primaryColor, selectedGameVersion, t]);
   return (
     <Modal
       scrollBehavior="inside"
@@ -149,39 +58,13 @@ export const DownloadGameServerModal: React.FC<
     >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{t("DownloadGameServerModal.header.title")}</ModalHeader>
+        <ModalHeader>
+          {t("AddAndImportInstancePage.moreOptions.server.title")}
+        </ModalHeader>
         <ModalCloseButton />
-        <Center>
-          <Stepper
-            colorScheme={primaryColor}
-            index={activeStep}
-            w="50%"
-            my={1.5}
-          >
-            {steps.map((step, index) => (
-              <Step key={index}>
-                <StepIndicator>
-                  <StepStatus
-                    complete={<StepIcon />}
-                    incomplete={<StepNumber />}
-                    active={<StepNumber />}
-                  />
-                </StepIndicator>
-                <Box flexShrink="0">
-                  <StepTitle fontSize="sm">
-                    {t(`DownloadGameServerModal.stepper.${step.key}`)}
-                  </StepTitle>
-                  <StepDescription fontSize="xs">
-                    {index < activeStep && step.description}
-                  </StepDescription>
-                </Box>
-                <StepSeparator />
-              </Step>
-            ))}
-          </Stepper>
-        </Center>
+
         <Flex h="60vh" flexDir="column">
-          {steps[activeStep].content}
+          {Content}
         </Flex>
       </ModalContent>
     </Modal>
