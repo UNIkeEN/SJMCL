d206ccb78d9585456ddaea00e0aff39d0763372b
baiyuansjtu <sun060319@sjtu.edu.cn>
2025-02-13 16:11:06 +0800

    refactor(frontend): adjust code

--- src/components/modals/screenshot-preview-modal.tsx
+++ src/components/modals/screenshot-preview-modal.tsx
@@ -35,9 +35,7 @@ const ScreenshotPreviewModal: React.FC<ScreenshotPreviewModalProps> = ({
           <Image
             src={screenshot.imgSrc}
             alt={screenshot.fileName}
-            borderRadius="lg"
-            minWidth="100%"
-            maxHeight="300px"
+            borderRadius="md"
             objectFit="cover"
           />
           <Flex justify="space-between" align="center" mt={2} px={4}>
@@ -47,16 +45,13 @@ const ScreenshotPreviewModal: React.FC<ScreenshotPreviewModalProps> = ({
 
             <HStack spacing={2}>
               <Icon as={LuCalendarDays} color="gray.500" />
-              <Text fontSize="sm" fontWeight="bold" color="black">
+              <Text fontSize="xs" fontWeight="bold" color="gray.500">
                 {ISOToDatetime(screenshot.time)}
               </Text>
               <CommonIconButton
                 icon="revealFile"
                 tooltipPlacement="top"
-                aria-label="Open Folder"
                 variant="ghost"
-                colorScheme="gray"
-                size="xs"
                 onClick={() => {
                   revealItemInDir(screenshot.filePath);
                 }}
