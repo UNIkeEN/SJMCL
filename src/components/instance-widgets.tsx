da0fc1439c3f9c7353b524a3c686c84f23076949
HsxMark <hsxmark@outlook.com>
2025-02-26 07:23:47 +0800

    fix(frontend): adjust code style

--- src/components/instance-widgets.tsx
+++ src/components/instance-widgets.tsx
@@ -229,20 +229,16 @@ export const InstanceModsWidget = () => {
         <HStack spacing={4}>
           <VStack align="flex-start" spacing={3}>
             <AvatarGroup size="sm" max={5} spacing={-2.5}>
-              {localMods
-                .slice()
-                .sort((a, b) => Number(b.enabled) - Number(a.enabled))
-                .map((mod, index) => (
-                  <Avatar
-                    key={index}
-                    name={mod.name || mod.fileName}
-                    src={base64ImgSrc(mod.iconSrc)}
-                    style={{
-                      filter: mod.enabled ? "none" : "grayscale(90%)",
-                      opacity: mod.enabled ? 1 : 0.5,
-                    }}
-                  />
-                ))}
+              {localMods.map((mod, index) => (
+                <Avatar
+                  key={index}
+                  name={mod.name || mod.fileName}
+                  src={base64ImgSrc(mod.iconSrc)}
+                  style={{
+                    filter: mod.enabled ? "none" : "grayscale(90%)",
+                  }}
+                />
+              ))}
             </AvatarGroup>
             <Text fontSize="xs" color="gray.500">
               {t("InstanceWidgets.mods.summary", { totalMods, enabledMods })}
