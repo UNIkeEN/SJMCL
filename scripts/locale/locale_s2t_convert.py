a1ce5a26de51b677bd7968eb496745f063312c9a
HsxMark <hsxmark@outlook.com>
2025-04-20 11:43:29 +0800

    fix(scripts): change file name, adjust comment

--- scripts/locale/locale_s2t_convert.py
+++ scripts/locale/locale_s2t_convert.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python3
 """
-This script converts Simplified Chinese text in a JSON file to Traditional Chinese text.
+This script converts zh-Hans locale to zh-Hant by OpenCC.
 
 Usage:
+    pip install opencc
     python convert_locale.py
-
-Description:
-    The script reads a source JSON file containing Simplified Chinese text, converts the text to Traditional Chinese, 
-    and writes the result to a new JSON file. It uses the OpenCC library for conversion and recursively processes all 
-    strings within nested dictionaries and lists in the JSON data.
-
-Parameters:
-    None. The script uses a predefined configuration:
-        - Input file: 'src/locales/zh-Hans.json' (Simplified Chinese)
-        - Output file: 'src/locales/zh-Hant.json' (Traditional Chinese)
-        - Conversion mode: 's2twp' (converts Simplified Chinese to Traditional Chinese with Hong Kong and Macau extensions)
-
-Dependencies:
-    opencc: A library for Chinese conversion between Simplified and Traditional characters.
-            Install it via pip: pip install opencc
-
-Note:
-    - Ensure the script is located in the correct directory structure relative to the source JSON file.
-    - The script assumes that the input JSON file uses UTF-8 encoding.
 """
-
-
 
 import json
 import os
@@ -41,7 +21,7 @@ def convert_simplified_to_traditional(obj):
     else:
         return obj
 
-converter = OpenCC('s2twp')
+converter = OpenCC('s2twp')  # Conversion mode: 's2twp' (Simplified Chinese to Traditional Chinese (Taiwan Standard) with Taiwanese idiom)
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(os.path.dirname(script_dir))
