2a10d93d8a613ceeb7ed95b2695580a6c9648511
Reqwey <linhk1606@outlook.com>
2025-06-04 21:13:45 +0800

    fix(modal): update player type initialization to depend on modal open state

--- src/components/modals/add-player-modal.tsx
+++ src/components/modals/add-player-modal.tsx
@@ -101,7 +101,7 @@ const AddPlayerModal: React.FC<AddPlayerModalProps> = ({
 
   useEffect(() => {
     setPlayerType(initialPlayerType);
-  }, [initialPlayerType, initialAuthServerUrl]);
+  }, [initialPlayerType, modalProps.isOpen]);
 
   useEffect(() => {
     let _authServer: AuthServer | undefined = undefined;
