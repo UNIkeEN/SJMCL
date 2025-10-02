0505ffe330ef9005215ec0b7c1a5d0699ca015aa
pynickle <2330458484@qq.com>
2025-10-02 15:07:13 +0800

    fix: refactor the logic

--- src/utils/wiki.ts
+++ src/utils/wiki.ts
@@ -9,44 +9,19 @@ export const getWikiLink = (
   version: GameClientResourceInfo
 ): string => {
   let wikiVersion = version.id;
-
-  let variantSuffix = "";
-  if (locale.startsWith("zh")) {
-    variantSuffix = locale.endsWith("Hant")
-      ? "?variant=zh-tw"
-      : "?variant=zh-cn";
-  }
-
-  if (version.gameType == "snapshot") {
-    return (
-      t("Utils.wiki.url-snapshot", {
-        version: wikiVersion,
-      }) + variantSuffix
-    );
-  } else {
-    if (wikiVersion.length >= 6 && wikiVersion.charAt(2) === "w") {
-      if (SNAPSHOT_PATTERN.test(wikiVersion)) {
-        if (wikiVersion === "22w13oneblockatatime") {
-          wikiVersion = "22w13oneBlockAtATime";
-        }
-        return (
-          t("Utils.wiki.url-snapshot", {
-            version: wikiVersion,
-          }) + variantSuffix
-        );
-      }
-    }
-
-    const lower = wikiVersion.toLocaleLowerCase(locale);
-
-    if (lower.startsWith("b")) {
-      wikiVersion = lower.replace("b", "Beta_");
-    }
-  }
-
-  return (
-    t("Utils.wiki.url", {
+  if (SNAPSHOT_PATTERN.test(wikiVersion)) {
+    return t("Utils.wiki.url-snapshot", {
       version: wikiVersion,
-    }) + variantSuffix
-  );
+    });
+  }
+
+  const lower = wikiVersion.toLocaleLowerCase(locale);
+
+  if (lower.startsWith("b")) {
+    wikiVersion = lower.replace("b", "Beta_");
+  }
+
+  return t("Utils.wiki.url", {
+    version: wikiVersion,
+  });
 };
