9c2b32bf2b4dafd643d2aa139afbbf27a577b8f7
xunying123 <xun_ying123@163.com>
2026-03-09 15:33:51 +0800

    fix

--- src-tauri/src/account/helpers/import/misc.rs
+++ src-tauri/src/account/helpers/import/misc.rs
@@ -1,22 +1,15 @@
 use crate::launcher_config::models::LauncherConfig;
+use crate::EXE_DIR;
 use std::collections::HashSet;
 use std::path::PathBuf;
-use std::sync::LazyLock;
 use std::sync::Mutex;
 use tauri::{AppHandle, Manager};
 
 pub const ACCESS_TOKEN_EXPIRED: &str = "%failed:access_token_expired%";
-static EXE_DIR: LazyLock<Option<PathBuf>> = LazyLock::new(|| {
-  std::env::current_exe()
-    .ok()
-    .and_then(|p| p.parent().map(|p| p.to_path_buf()))
-});
 
 pub fn find_game_dirs(app: &AppHandle) -> Vec<PathBuf> {
   let mut dirs = HashSet::new();
-  if let Some(exe_dir) = &*EXE_DIR {
-    dirs.insert(exe_dir.clone());
-  }
+  dirs.insert(EXE_DIR.clone());
   let local_game_directories = {
     let binding = app.state::<Mutex<LauncherConfig>>();
     let state = binding.lock().unwrap();
