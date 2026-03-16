1e62c7458fb881d851b01213b2279b6bf0be9bff
Junhua Wang <hsxmark@outlook.com>
2026-03-16 23:48:06 +0800

    fix(frontend): fix select instance and player modal error

--- src/components/modals/select-instance-modal.tsx
+++ src/components/modals/select-instance-modal.tsx
@@ -28,15 +28,21 @@ const SelectInstanceModal: React.FC<SelectInstanceModalProps> = ({
   const { t } = useTranslation();
 
   return (
-    <Modal size="md" {...modalProps}>
+    <Modal size="md" scrollBehavior="inside" {...modalProps}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent maxH="80vh" pb={4}>
         <ModalHeader>
           {modalTitle || t("SelectInstanceModal.header.title")}
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody pb={6}>
+        <ModalBody pb={6} minH={0}>
           <InstancesView
+            h="100%"
+            overflowY="auto"
+            overflowX="hidden"
+            onWheel={(e) => {
+              e.stopPropagation();
+            }}
             instances={candidateInstances}
             selectedInstance={selectedInstance}
             viewType="list"
--- src/components/modals/select-player-modal.tsx
+++ src/components/modals/select-player-modal.tsx
@@ -28,15 +28,22 @@ const SelectPlayerModal: React.FC<SelectPlayerModalProps> = ({
   const { t } = useTranslation();
 
   return (
-    <Modal size="md" {...modalProps}>
+    <Modal size="md" scrollBehavior="inside" {...modalProps}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent maxH="80vh" pb={4}>
         <ModalHeader>
           {modalTitle || t("SelectPlayerModal.header.title")}
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody pb={6}>
+        <ModalBody pb={6} minH={0}>
           <PlayersView
+            h="100%"
+            maxH="60vh"
+            overflowY="auto"
+            overflowX="hidden"
+            onWheel={(e) => {
+              e.stopPropagation();
+            }}
             players={candidatePlayers}
             selectedPlayer={undefined}
             viewType="list"
