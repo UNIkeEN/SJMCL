d215600c66ff0cf18b75650c7b5592ca5747f97f
Xun_Ying <119675097+xunying123@users.noreply.github.com>
2026-06-15 15:43:40 +0800

    fix(backend): allow Linux removable drive run paths [skip ci] (#1727)

--- src-tauri/src/launcher_config/helpers/misc.rs
+++ src-tauri/src/launcher_config/helpers/misc.rs
@@ -217,7 +217,8 @@ pub fn check_exe_path_availability(app: &AppHandle) -> bool {
       || exe_str.starts_with("/var/tmp/")
       || exe_str.starts_with("/var/cache/")
       || exe_str.starts_with("/dev/shm/")
-      || exe_str.starts_with("/run/")
+      // /run is tmpfs, while /run/media is a common mount point on some Linux distributions.
+      || (exe_str.starts_with("/run/") && !exe_str.starts_with("/run/media/"))
       || exe_str.contains("/Trash/"))
   }
 
