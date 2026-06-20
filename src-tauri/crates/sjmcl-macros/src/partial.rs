aca317a8ada85361a362c39b7c78e46ac00f02c3
Yiyang Wu <toolmanp@tlmp.cc>
2026-06-20 15:02:39 +0800

    refactor(backend): move towards workspace layout
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- src-tauri/crates/sjmcl-macros/src/partial.rs
+++ src-tauri/crates/sjmcl-macros/src/partial.rs
@@ -2,13 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::DeriveInput;
 
-#[proc_macro_derive(Partial)]
-pub fn derive_partial(input: TokenStream) -> TokenStream {
-  let derived_input = syn::parse_macro_input!(input as DeriveInput);
-  derive_partial_traits(&derived_input)
-}
-
-fn derive_partial_traits(input: &syn::DeriveInput) -> TokenStream {
+pub(crate) fn derive_partial_impl(input: &DeriveInput) -> TokenStream {
   let ident = &input.ident;
   let fields = match &input.data {
     syn::Data::Struct(data) => &data.fields,
