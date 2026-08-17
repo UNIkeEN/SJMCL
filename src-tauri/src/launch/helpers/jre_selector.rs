9b15bbd70729820a6d90898cd5db596d33a2ca4b
辞庐 <109708109+CiiLu@users.noreply.github.com>
2026-08-17 09:47:58 +0800

    ?! java !?

--- src-tauri/src/launch/helpers/jre_selector.rs
+++ src-tauri/src/launch/helpers/jre_selector.rs
@@ -29,6 +29,22 @@ pub async fn select_java_runtime(
 
   let mut min_version_req = get_minimum_java_version_by_game(app, instance, true).await;
 
+  if instance.mod_loader.loader_type == Cleanroom {
+    let parsed_cleanroom_version = semver::Version::parse(instance.mod_loader.version.as_ref());
+    let minium_25_cleanroom_version = semver::Version::parse("0.5.0-alpha");
+    if let Ok(parsed) = parsed_cleanroom_version
+      && let Ok(minium_25) = minium_25_cleanroom_version
+    {
+      if parsed.cmp(&minium_25).is_lt() {
+        min_version_req = 21;
+      } else {
+        min_version_req = 25;
+      }
+    } else {
+      min_version_req = 25;
+    };
+  }
+
   if client_json_req > min_version_req {
     min_version_req = client_json_req;
   }
@@ -57,22 +73,6 @@ pub async fn get_minimum_java_version_by_game(
   instance: &Instance,
   fallback_fetch_remote: bool,
 ) -> i32 {
-  if instance.mod_loader.loader_type == Cleanroom {
-    let parsed_cleanroom_version = semver::Version::parse(instance.mod_loader.version.as_ref());
-    let minium_25_cleanroom_version = semver::Version::parse("0.5.0-alpha");
-    return if let Ok(parsed) = parsed_cleanroom_version
-      && let Ok(minium_25) = minium_25_cleanroom_version
-    {
-      if parsed.cmp(&minium_25).is_lt() {
-        21
-      } else {
-        25
-      }
-    } else {
-      25
-    };
-  }
-
   // only allow fallback remote fetch here in the launch process, as Java selection and command generation are used sequentially.
   // ref: https://github.com/UNIkeEN/SJMCL/pull/799
   // 26.1(26.1-snapshot-1)+
