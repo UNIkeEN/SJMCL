b068e3e9c2c680d14d73ca691b4bdce87c4df197
辞庐 <109708109+CiiLu@users.noreply.github.com>
2026-09-26 22:31:51 +0800

    update

--- src-tauri/src/instance/helpers/loader/common.rs
+++ src-tauri/src/instance/helpers/loader/common.rs
@@ -8,6 +8,7 @@ use tauri::AppHandle;
 use zip::ZipArchive;
 
 use crate::instance::helpers::client_json::{LibrariesValue, McClientInfo};
+use crate::instance::helpers::game_version::compare_game_versions;
 use crate::instance::helpers::loader::cleanroom::install_cleanroom_loader;
 use crate::instance::helpers::loader::fabric::install_fabric_loader;
 use crate::instance::helpers::loader::forge::{InstallProfile, install_forge_loader};
@@ -76,7 +77,10 @@ pub async fn install_mod_loader(
       .await
     }
     ModLoaderType::Forge => {
-      if Version::parse(game_version)?.lt(&Version::new(1, 6, 0)) {
+      if compare_game_versions(&app, game_version, "1.6.0", false)
+        .await
+        .is_lt()
+      {
         install_universal_forge_loader(priority, game_version, loader, lib_dir.clone(), task_params)
           .await
       } else {
